66fb1fccf53ae731ef56befa49c06a4de7cd1919
anilreddy4438 <anilkumar190293@gmail.com>
2016-01-23 17:32:00 +0530

    push1

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,25 @@ app.controller('studentLoginController',['$kinvey','$scope',function($kinvey,$sc
         })
     }
 }]);
-app.controller('studentsignupController',['$kinvey','$scope',function($kinvey,$scope){
-    console.log('studentsignup1235462') }])
+app.controller('studentSignupController',['$kinvey','$scope',function($kinvey,$scope){
+    console.log('studentsignup1235462')
+
+$scope.signup = function($event){
+    $event.preventDefault();
+    console.log($scope.student)
+    console.log('student signed up');
+    var promise = $kinvey.User.signup(
+        $scope.student
+    );
+    promise.then(function(user) {
+console.log(user);
+        console.log('user created');
+    }, function(err) {
+        console.log(err)
+    });
+}
+
+
+}])
 
 
