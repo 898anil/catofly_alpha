b495b4688a5759b6570da0e38f65acf4634c7f34
Anil Kumar Nayak <898anil@gmail.com>
2016-01-25 12:13:36 +0530

    vey

---
+++ assets/js/controllers/studentLoginController.js
@@ -0,0 +1,24 @@
+app.controller('studentLoginController',['$kinvey','$scope',function($kinvey,$scope){
+    $scope.studentLogin = function($event){
+        $event.preventDefault();
+        console.log('form submitted');
+        console.log($scope.student);
+        var login = $kinvey.User.login({username:$scope.student.username,password:$scope.student.password});
+        login.then(function(user){
+            console.log('user logged in' + user._id);
+            location.href="catofly/views/catofly-dashboard2.html";
+        },function(err){
+            console.log(err.message);
+            $scope.loginError = "Incorrect username or password";
+
+        })
+
+    }
+    $scope.studentlogout = function(){
+        Kinvey.User.logout();
+    }
+    initpromise.then(function(){
+        $scope.userloggedin = Kinvey.getActiveUser();
+    })
+
+}]);
