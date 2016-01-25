d8abf1fa1e75333c810fc9fc32c0db9f88379073
anilreddy4438 <anilkumar190293@gmail.com>
2016-01-25 15:55:54 +0530

    signup page data management

--- assets/js/controllers/studentSignupController.js
+++ assets/js/controllers/studentSignupController.js
@@ -1,18 +1,33 @@
-/**
- * Created by jellyglass on 25/1/16.
- */
+var studentsignupId ;
 app.controller('studentSignupController',['$kinvey','$scope',function($kinvey,$scope){
-    console.log('studentsignup1235462');
+    console.log('studentsignedup');
     $scope.signup = function($event){
         $event.preventDefault();
-        console.log($scope.student)
+        console.log($scope.student);
+
+
         console.log('student signed up');
-        var promise = $kinvey.User.signup(
-            $scope.student
-        );
-        promise.then(function(user) {
-            console.log(user);
+        var signup = $kinvey.User.signup({
+
+            username  : $scope.student.username,
+            password: $scope.student.password });
+        signup.then(function(user) {
+            console.log(user._id);
             console.log('user created');
+            studentsignupId = user._id;
+            var studentDatalist = {};
+            studentDatalist = $scope.student;
+            studentDatalist._id = studentsignupId
+            console.log(studentDatalist)
+
+            var studentsave = $kinvey.DataStore.save('studentUserlist',studentDatalist);
+            studentsave.then(function(model) {
+                console.log(model._id)
+            }, function(err) {
+                console.log(err);
+            });
+
+            //cns
         }, function(err) {
             console.log(err);
         });
